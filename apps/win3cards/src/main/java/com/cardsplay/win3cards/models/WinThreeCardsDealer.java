200454dff512e5596b77addc17abe83e4d9628e7
henry jiang <blackmoon88@126.com>
2018-09-24 21:51:25 +0800

    add life cycle management interface

--- apps/win3cards/src/main/java/com/cardsplay/win3cards/models/WinThreeCardsDealer.java
+++ apps/win3cards/src/main/java/com/cardsplay/win3cards/models/WinThreeCardsDealer.java
@@ -12,7 +12,6 @@ public class WinThreeCardsDealer extends Dealer {
 
     public WinThreeCardsDealer(DealType dealType) {
         super(dealType);
-        // TODO Auto-generated constructor stub
     }
 
     @Override
