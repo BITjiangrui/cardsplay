bda5275ebf1227b18c500014453c13a81cbe544f
jiangrui <jiangrui@didichuxing.com>
2018-10-06 12:37:16 +0800

    add rule in room

--- core/src/main/java/com/cardsplay/core/models/Rule.java
+++ core/src/main/java/com/cardsplay/core/models/Rule.java
@@ -4,10 +4,13 @@ public enum Rule {
 
     NEWBEE(new Bet(0.1), 2,10),  EXPERT(new Bet(0.5), 10, 50), MASTER(new Bet(1),20, 100);
 
+    // 单注
     private  Bet singleBet;
 
+    // 单回合最高下注
     private  int upperLimit;
 
+    // 钱包余额
     private int balance;
 
     private Rule(Bet singleBet, int upperLimit, int balance){
